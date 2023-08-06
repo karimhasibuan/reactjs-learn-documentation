efa3aa6d704a4a5374a892fc51d9ecf185a32300
Karim Hasibuan <amfibicoding19@gmail.com>
2023-08-06 16:00:09 +0700

    feat: use external css  to app.js in  react app.

---
+++ 11_style/eksternal_css/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 className='judul'>CSS Stylesheet</h2>
+                <div className='kotak'></div>
+            </div>
+        );
+    }
+}
+
+export default App;
