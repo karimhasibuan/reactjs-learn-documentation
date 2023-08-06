3f38f6bfcdc9f6cf8ea57ba297324c8b76834dc1
Karim Hasibuan <amfibicoding19@gmail.com>
2023-08-06 16:07:55 +0700

    feat: use module css to component.

--- 11_style/eksternal_css/src/App.js
+++ 11_style/eksternal_css/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.css'; // use external css file for styling
+import styles from './App.module.css'; // use module css for styling element
 
 class App extends React.Component {
     render() {
@@ -7,6 +8,7 @@ class App extends React.Component {
             <div>
                 <h2 className='judul'>CSS Stylesheet</h2>
                 <div className='kotak'></div>
+                <div className={styles.lingkaran}></div> // implement styling from module css
             </div>
         );
     }
